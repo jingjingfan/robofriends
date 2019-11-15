bd4ff1895e5ad32b100edef5b7ec8d7b2c3f42d4
Jing Fan <jingjingfan@gmail.com>
2019-11-15 12:17:51 -0500

    Updates

---
+++ precache-manifest.040cca8b3689c9226aaa871212fbaf93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a679dc13909aa6567a4177e3f1f28103",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e0571a1f220115ae4aa8",
+    "url": "/robofriends/static/css/main.7c2d0133.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriends/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "e0571a1f220115ae4aa8",
+    "url": "/robofriends/static/js/main.a7b53099.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
